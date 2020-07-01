4aef181c6d8d2b1630658f0d2c9b813427c0ab3d
Prizrak <60360057+Prizrak11@users.noreply.github.com>
2020-06-30 23:46:53 -0500

    created the page without the info and a responsive design

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+/* Scroll Reveal */
+ScrollReveal().reveal('.carousel',{delay:"100", distance:"5em", origin:"top"});
+ScrollReveal().reveal('.mini-us',{delay:"100", distance:"5em", origin:"left"})
+ScrollReveal().reveal('.services-main')
+ScrollReveal().reveal('.part-title',{delay:"200", distance:"5em", origin:"right"})
+ScrollReveal().reveal('.card',{delay:"300", distance:"5em", origin:"bottom"})
+ScrollReveal().reveal('.slider',{delay:"200", distance:"5em", origin:"right"})
+ScrollReveal().reveal('.info-card',{delay:"200", distance:"10em", origin:"right"});
+ScrollReveal().reveal('.reverse',{delay:"200", distance:"10em", origin:"left"});
+ScrollReveal().reveal(`.tab`,{delay:"400", distance:"5em", origin:"bottom"})
+ScrollReveal().reveal('.gmap_canvas',{delay:"300", distance:"5em", origin:"top"});
+ScrollReveal().reveal('.info-description',{delay:"100", distance:"5em", origin:"top"});
+ScrollReveal().reveal('.values',{delay:"200", distance:"10em", origin:"right"});
